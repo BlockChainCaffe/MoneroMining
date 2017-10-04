322d07358684d1782de7821b94d08bec79aae6e4
Emcysquare <marco.crotta@gmail.com>
2017-10-04 11:48:39 +0200

    corretto indirizzo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #
 ########################################################
 
-WALLET_ADDRESS=42WaLsGbVXA4xRtDw7JgZXPCQgB7ZqzPWUDHY3TiP6TM6vyWV1iMx3hdUo5CmR2eCqRh6MkHj7viJb2vJdsfiY5rgCnmAfsP
+WALLET_ADDRESS=42WaLsGbVXA4xRtDw7JgZXPCQgB7ZqzPWUDHY3TiP6TM6vyWV1iMx3hdUo5CmR2eCqRh6MkHj7viJb2vJdsfY5rgCnmAfsP
 CPU=$(cat /proc/cpuinfo | grep processor | wc -l)
 [[ $CPU > 3 ]] && CPU=$(( CPU - 2 )) || CPU=1 
 
